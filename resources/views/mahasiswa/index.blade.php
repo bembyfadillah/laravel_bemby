dd6a2c9e1cf16f1c859b841c79082bb2e50ee04b
bemby fadillah <bemby@ft-umt.ac.id>
2020-07-22 23:16:38 +0700

    Pert 11 : Membuat Halaman Template

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Halaman Mahasiswa')
+@section('bread1', 'Mahasiswa')
+@section('bread2', 'Daftar')
+
+
+@section('content')
+<p> This is my body content.</p>
+@endsection
