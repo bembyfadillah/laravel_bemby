e8cd54714a55b9cd2c00ea05535ab43d5733a62f
bemby fadillah <bemby@ft-umt.ac.id>
2020-07-23 14:14:02 +0700

    Pert 12: Install Datatables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>@yield('title')</title>
-	<script src="{{asset('js/app.js') }}"></script>
 	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
+	<link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+	<script src="{{asset('js/app.js') }}"></script>
+	<script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 </head>
 <body>
 	@include('layouts.navbar')
-	<div class="container.fluid">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="row mt-1">
 			<div class="col-md-2">
-			@include('layouts.sidebar')
+				@include('layouts.sidebar')
 			</div>
 			<div class="col-md-10">
 				@include('layouts.breadcrumb')
